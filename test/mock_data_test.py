44eab393364065e86eaca92e39bbf694fdd19874
davyd <Davyd Mykytyn>
2018-08-22 19:02:27 -0400

    Fixing test for multiband

--- test/mock_data_test.py
+++ test/mock_data_test.py
@@ -4,8 +4,8 @@ import justice.simulate as sim
 
 
 def test_make_gauss():
-    gauss_fcn = sim.make_gauss(1.0)
-    xs = np.arange(0.0, 1.0, 0.1)
+    gauss_fcn = sim.make_gauss([1.0,])
+    xs = [np.arange(0.0, 1.0, 0.1),]
     ys = gauss_fcn(xs)
     expected = [1.,
                 0.99501248,
@@ -17,15 +17,15 @@ def test_make_gauss():
                 0.78270454,
                 0.72614904,
                 0.66697681]
-    assert np.sum(np.abs(expected - ys)) < 1e-6
+    assert np.sum(np.abs(expected - ys[0])) < 1e-6
 
 
 def test_make_dataset():
     num_obj = 10
     cls_models = [sim.make_gauss, sim.make_sine]
-    cls_params = [{'scale': 10., 'loc': 100., 'amp': 50., 'const': 1.},
-                  {'period': 20., 'phase': 0., 'amp': 5., 'const': 5.}]
+    cls_params = [{'scales': [10.], 'locs': [100.], 'amps': [50.], 'consts': [1.]},
+                  {'periods': [20.], 'phases': [0.], 'amps':[5.], 'consts': [5.]}]
     cls_wts = None  # even split for now
-    def_cadence = np.arange(0., 200., 5.)
+    def_cadence = [np.arange(0., 200., 5.),]
     sim.make_dataset(num_obj, def_cadence, cls_models, cls_params, cls_wts=None)
     return
