df09a029bb5e3394b89d615c73c4a5468369aa6b
davyd <dwm261@nyu.edu>
2018-08-22 22:40:08 -0400

    remove testing print statements

--- justice/lightcurve.py
+++ justice/lightcurve.py
@@ -12,14 +12,11 @@ def merge(lca, lcb):
     ord_x = []
     ord_y = []
     ord_yerr = []
-    print (lca, lcb)
     for i,j in enumerate(lca.x):
-        print (lca.x[i], lcb.x[i], lca.y[i], lcb.y[i])
         new_x = np.concatenate((lca.x[i], lcb.x[i]))
         new_y = np.concatenate((lca.y[i], lcb.y[i]))
         new_yerr = np.concatenate((lca.yerr[i], lcb.yerr[i]))
         order = np.argsort(new_x)
-        print (len(new_x), len(new_y), len(new_yerr))
         ord_x.append(new_x[order])
         ord_y.append(new_y[order])
         ord_yerr.append(new_yerr[order])
