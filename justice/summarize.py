94c8fb1ce92c447ea4af24dd3a7c4ebe58f6be1c
davyd <dwm261@nyu.edu>
2018-09-02 17:50:51 -0400

    style changes

--- justice/summarize.py
+++ justice/summarize.py
@@ -20,14 +20,24 @@ def connect_the_dots(lc):
     for x, y, yerr in zip(lc.x.T, lc.y.T, lc.yerr.T):
         x_difs = (x[1:] - x[:-1])
         y_difs = y[1:] - y[:-1]
-        sol += np.sum(np.sqrt(x_difs ** 2 + y_difs ** 2))
+        sol += np.sum(np.sqrt(x_difs**2 + y_difs**2))
     return sol
 
 
-def opt_arclen(lca, lcb, ivals=np.array([0., 0., 1., 1.]), constraints=[], method='Nelder-Mead', options={'maxiter':10000}, vb=True):
+def opt_arclen(
+    lca,
+    lcb,
+    ivals=np.array([0., 0., 1., 1.]),
+    constraints=[],
+    method='Nelder-Mead',
+    options={'maxiter': 10000},
+    vb=True
+):
     if method != 'Nelder-Mead':
+
         def pos_dil(aff):
-            return(min(aff.dx, aff.dy))
+            return (min(aff.dx, aff.dy))
+
         constraints += [{'type': 'ineq', 'fun': pos_dil}]
     else:
         constraints = None
@@ -38,32 +48,50 @@ def opt_arclen(lca, lcb, ivals=np.array([0., 0., 1., 1.]), constraints=[], metho
         lc = transform(lcb, aff)
         new_lc = merge(lca, lc)
         length = connect_the_dots(new_lc)
-        return(length)
-    # could make this a probability by taking chi^2 error relative to connect_the_dots original, but it didn't work better in the sandbox notebook
-    res = spo.minimize(_helper, ivals, constraints=constraints, method=method, options=options)
+        return (length)
+
+    # could make this a probability by taking chi^2 error relative to
+    # connect_the_dots original, but it didn't work better in the sandbox
+    # notebook
+    res = spo.minimize(
+        _helper, ivals, constraints=constraints, method=method, options=options
+    )
     if vb:
         print(res)
     res_aff = Aff(*res.x)
-    return(res_aff)
+    return (res_aff)
 
 
 def fit_gp(lctrain, xpred, kernel=None):
-    gp = GPy.models.gp_regression.GPRegression(lctrain.x, lctrain.y,normalizer=True)
+    gp = GPy.models.gp_regression.GPRegression(lctrain.x, lctrain.y, normalizer=True)
     gp.optimize()
-    
+
     ypred, yvarpred = gp.predict(xpred)
     lcpred = LC(xpred, ypred, np.sqrt(yvarpred))
     fin_like = gp.log_likelihood()
-    return(lcpred, fin_like)
+    return (lcpred, fin_like)
 
-def opt_gp(lca, lcb, ivals=np.array([0., 0., 1., 1.]), constraints=[], method='Nelder-Mead', options={'maxiter':10000}, vb=True):
+
+def opt_gp(
+    lca,
+    lcb,
+    ivals=np.array([0., 0., 1., 1.]),
+    constraints=[],
+    method='Nelder-Mead',
+    options={'maxiter': 10000},
+    vb=True
+):
     if method != 'Nelder-Mead':
+
         def pos_dil(aff):
-            return(min(aff.dx, aff.dy))
+            return (min(aff.dx, aff.dy))
+
         constraints += [{'type': 'ineq', 'fun': pos_dil}]
     else:
         constraints = None
-    def_cadence=np.vstack((np.arange(0., 1000., 10.),np.arange(0., 1000., 10.))).T #Not general! Need to change.
+    # Not general! Need to change.
+    def_cadence = np.vstack((np.arange(0., 1000., 10.), np.arange(0., 1000., 10.))).T
+
     # don't know if this way of handling constraints actually works -- untested!
     def _helper(vals):
         aff = Aff(*vals)
@@ -71,10 +99,14 @@ def opt_gp(lca, lcb, ivals=np.array([0., 0., 1., 1.]), constraints=[], method='N
         new_lc = merge(lca, lc)
         pred, fin_like = fit_gp(new_lc, def_cadence)
         return (-fin_like)
-    # could make this a probability by taking chi^2 error relative to connect_the_dots original, but it didn't work better in the sandbox notebook
-    res = spo.minimize(_helper, ivals, constraints=constraints, method=method, options=options)
+
+    # could make this a probability by taking chi^2 error relative to
+    # connect_the_dots original, but it didn't work better in the sandbox
+    # notebook
+    res = spo.minimize(
+        _helper, ivals, constraints=constraints, method=method, options=options
+    )
     if vb:
         print(res)
     res_aff = Aff(*res.x)
-    return(res_aff)
-    
+    return (res_aff)
