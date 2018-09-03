a14863f17f0fbc9c8be525d692e0274157125d7b
zora tung <gatoatigrado@gmail.com>
2018-09-03 15:55:54 -0400

    Add tests and stuff

--- justice/supernova_data.py
+++ justice/supernova_data.py
@@ -65,28 +65,15 @@ def make_parse_fcn(truth_filename):
     return parse_file
 
 
-def generate_data_main():
-    cmd_args = argparse.ArgumentParser()
-    cmd_args.add_argument(
-        "--truth-file",
-        default=None,
-        help="File for an answer key, with lines like 'DES_SN____.DAT <id>''"
-    )
-    cmd_args.add_argument("input_dirs", nargs="+")
-    args = cmd_args.parse_args()
-    dat_files = [
-        filename  # don't auto-format
-        for input_dir in args.input_dirs
-        for filename in glob.glob(os.path.join(input_dir, "*.DAT"))
-    ]
-
+def generate_binary_data(dat_files, truth_file, override_index_filename, override_all_lc_data,
+                         print_status=True):
     index_rows = []
     all_values = []  # will be concatenated.
     next_start = 0
-    parse_file = make_parse_fcn(args.truth_file)
+    parse_file = make_parse_fcn(truth_file)
     for i, dat_file in enumerate(dat_files):
         sn_id, sn_type, by_flt = parse_file(dat_file)
-        if i % 10 == 0:
+        if print_status and i % 10 == 0:
             print(".", end="", file=sys.stderr)
             sys.stderr.flush()
         for flt, values in by_flt.items():
@@ -100,23 +87,80 @@ def generate_data_main():
                 'end': next_start,
             })
             all_values.append(values)
-    print(file=sys.stderr)
+    if print_status:
+        print(file=sys.stderr)
     data = np.concatenate(all_values, axis=0)
     assert data.shape[0] == next_start
     assert data.shape[0] == index_rows[-1]['end']
-
     # Write index and data.
     index_df = pd.DataFrame.from_records(index_rows)
-    with open(index_filename, 'wb') as f:
+    with open(override_index_filename, 'wb') as f:
         pickle.dump(index_df, f, protocol=2)
-    all_lc_data.write(data)
+    override_all_lc_data.write(data)
+
+
+def generate_data_main():
+    cmd_args = argparse.ArgumentParser()
+    cmd_args.add_argument(
+        "--truth-file",
+        default=None,
+        help="File for an answer key, with lines like 'DES_SN____.DAT <id>''"
+    )
+    cmd_args.add_argument("input_dirs", nargs="+")
+    args = cmd_args.parse_args()
+    truth_file = args.truth_file
+    dat_files = [
+        filename  # don't auto-format
+        for input_dir in args.input_dirs
+        for filename in glob.glob(os.path.join(input_dir, "*.DAT"))
+    ]
+    override_index_filename = index_filename
+    override_all_lc_data = all_lc_data
+
+    generate_binary_data(dat_files, truth_file, override_index_filename, override_all_lc_data)
+
+
+def format_dense_multi_band_from_lc_dict(lc_dict, band_order=('g', 'r', 'i', 'z')):
+    """Formats a multi-band LC dictionary to a dense dataset.
+
+    Currently reformats a time series to dense data, as if every curve had sampled at the same time.
+    This method is currently slow.
+
+    :param lc_dict: Dictionary from lc_dict_for_id.
+    :param band_order: Order of expected bands.
+    :return: lightcurve.LC object.
+    """
+    from justice import lightcurve
+    if frozenset(lc_dict.keys()) != frozenset(band_order):
+        raise ValueError("Unexpected keys {}".format(lc_dict.keys()))
+
+    def closest_in_time(band, time):
+        index = np.argmin(np.abs(band[:, 0] - time))
+        # [3]-shaped array
+        return band[index]
+
+    bands = [lc_dict[band] for band in band_order]
+
+    # [num_points, num_bands, 3]-shaped array
+    dense_data = np.array([
+        [closest_in_time(band, time) for band in bands]
+        for time in bands[0][:, 0]
+    ], dtype=np.float64)
+    return lightcurve.LC(x=dense_data[:, :, 0], y=dense_data[:, :, 1], yerr=dense_data[:, :, 2])
 
 
 class SNDataset(object):
-    def __init__(self):
-        with open(index_filename, 'rb') as f:
+    """API over dataset for supernovae.
+
+
+    Attributes:
+      ids_with_answers: IDs with any answers.
+    """
+
+    def __init__(self, override_index_filename=index_filename, override_all_lc_data=all_lc_data):
+        with open(override_index_filename, 'rb') as f:
             self.index_df = pickle.load(f)
-        self.lc_data = all_lc_data.read()
+        self.lc_data = override_all_lc_data.read()
         self.rng = np.random.RandomState()  # Pre-init for faster sampling.
         self.all_ids = self.index_df['id'].unique()
         self.ids_with_answers = (
@@ -128,16 +172,37 @@ class SNDataset(object):
         data = self.lc_data[row.start:row.end, :]
         return row, data
 
-    def random_lc_all_flux(self, with_answers=True):
-        if with_answers:
-            random_id = self.rng.choice(self.ids_with_answers)
-        else:
-            random_id = self.rng.choice(self.all_ids)
+    def lcs_for_id(self, id_):
+        """Get light curves for an ID.
+
+        :param id_: ID to get data for.
+        :return: (index_row_namedtuple, subarray) pairs.
+            subarray is a [num_points, 3]-shaped array.
+        """
         row_and_data = []
-        for row in self.index_df[self.index_df['id'] == random_id].itertuples():
+        for row in self.index_df[self.index_df['id'] == id_].itertuples():
             data = self.lc_data[row.start:row.end, :]
             row_and_data.append((row, data))
-        return random_id, row_and_data
+        return row_and_data
+
+    def lc_dict_for_id(self, id_):
+        """Returns a dictionary mapping band to light curve array.
+
+        :param id_: ID to get data for.
+        :return: Dict from band ('i', 'r', etc.) to [num_points, 3]-shaped array.
+            The first column is time, second is flux, third is flux error.
+        """
+        row_and_data = self.lcs_for_id(id_)
+        result = {row.flt: array for row, array in row_and_data}
+        if len(row_and_data) != len(result):
+            raise ValueError("band does not uniquely identify sub-ranges")
+        return result
+
+    def random_answer_id(self):
+        return self.rng.choice(self.ids_with_answers)
+
+    def random_id(self):
+        return self.rng.choice(self.all_ids)
 
 
 if __name__ == '__main__':
---
+++ test/supernova_data_test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Test that SNDataset API functions well."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from justice import supernova_data
+
+
+def test_random_id_functions(sn_dataset):
+    """
+    :type sn_dataset: justice.supernova_data.SNDataset
+    :param sn_dataset: Supernova dataset fixture.
+    """
+    random_ids = frozenset(
+        sn_dataset.random_answer_id()
+        for _ in range(10)
+    )
+    assert random_ids == frozenset([92234, 197655])
+
+
+def test_lc_dict_for_id(sn_dataset):
+    """
+    :type sn_dataset: justice.supernova_data.SNDataset
+    :param sn_dataset: Supernova dataset fixture.
+    """
+    dct = sn_dataset.lc_dict_for_id(92234)
+    assert set(dct.keys()) == {'g', 'r', 'i', 'z'}
+    for val in dct.values():
+        assert isinstance(val, np.ndarray)
+        entries, num_columns = val.shape
+        assert 0 < entries < 100
+        assert num_columns == 3
+
+
+def test_format_dense_multi_band_lc(sn_dataset):
+    dct = sn_dataset.lc_dict_for_id(92234)
+    lc = supernova_data.format_dense_multi_band_from_lc_dict(dct)
+    first_times_all_channels = lc.x[0]
+    assert (
+                   np.max(first_times_all_channels) - np.min(first_times_all_channels)) < 1.0
+    assert 56177.0 < first_times_all_channels[0] < 56179.0
