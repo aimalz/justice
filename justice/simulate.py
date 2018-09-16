0d005147725bed0148f3ec930493caad1ef98352
Ted Singer <tedsinger1@gmail.com>
2018-09-16 19:27:51 -0400

    add TestLC

--- justice/simulate.py
+++ justice/simulate.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.stats as sps
-from justice.lightcurve import BandData
+from justice.lightcurve import BandData, _LC
 
 
 def make_cadence(xs, xerrs):
@@ -40,3 +40,30 @@ def make_dataset(num_obj, xs, shape_fn, cls_wts=None):
         bands.append(BandData.from_cadence_shape_and_errfracs(cadence, cls, np.array([0.1] * len(xs))))
 
     return bands
+
+
+class TestLC(_LC):
+    @property
+    def _expected_bands(self):
+        return 'b'
+
+    @classmethod
+    def make_super_easy(cls):
+        band = BandData(np.array([2, 3]), np.array([5, 6]), np.array([1, 1]))
+        return TestLC(b=band)
+
+    @classmethod
+    def make_easy_gauss(cls):
+        gauss_fcn = make_gauss_shape_fn(1.0, 0, 1, 0)
+        xs = make_cadence(np.arange(0, 22.6, .1), [0.] * 226)
+
+        band = BandData.from_cadence_shape_and_errfracs(xs, gauss_fcn, [0] * 226)
+        return TestLC(b=band)
+
+    @classmethod
+    def make_hard_gauss(cls):
+        gauss_fcn = make_gauss_shape_fn(1.5, 1, 1.2, .3)
+        xs = make_cadence(np.arange(0, 11.5, .1), [0.1] * 115)
+
+        band = BandData.from_cadence_shape_and_errfracs(xs, gauss_fcn, [0.2] * 115)
+        return TestLC(b=band)
