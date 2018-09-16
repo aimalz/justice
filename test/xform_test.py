674ff014f0809ec59ba7b417a882a09d08bdc71a
davyd <dwm261@nyu.edu>
2018-09-16 16:00:02 -0400

    affine -> xform

--- test/xform_test.py
+++ test/xform_test.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
-"""Tests for the affine transformation."""
+"""Tests for the transformation."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import numpy as np
 
-from justice import affine_xform, lightcurve
+from justice import xform, lightcurve
 
 
 def test_defaulting():
-    tf = affine_xform.Aff(tx=3.0)
+    tf = xform.Aff(tx=3.0)
     assert tf.ty == 0.0
     assert tf.dx == 1.0
     assert tf.dy == 1.0
-    affine_xform.Aff()  # Make sure defaults-only xform is ok.
+    xform.Aff()  # Make sure defaults-only xform is ok.
 
 
 def test_to_and_from_array():
-    tf = affine_xform.Aff(tx=0.1, ty=0.2, dx=1.0, dy=2.0)
+    tf = xform.Aff(tx=0.1, ty=0.2, dx=1.0, dy=2.0)
     params = tf.as_array()
-    tf2 = affine_xform.make_aff(params)
+    tf2 = xform.make_aff(params)
     assert tf == tf2
 
 
 def test_transform():
-    tf = affine_xform.Aff(tx=-3215, ty=0.2, dx=10.0, dy=2.0)
+    tf = xform.Aff(tx=-3215, ty=0.2, dx=10.0, dy=2.0)
     lca = lightcurve.LC(
         x=np.array([[3215], [3217]], dtype=np.float64),
         y=np.array([[12], [17]], dtype=np.float64),
