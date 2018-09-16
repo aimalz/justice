f1a2a7048cdef9cb1aa82a0a713db97980bcb58a
zora tung <gatoatigrado@gmail.com>
2018-09-16 17:08:37 -0400

    New cells for visualizing OGLE, add star table

--- justice/ogle_data.py
+++ justice/ogle_data.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import random
 import os.path
 
 from justice import mmap_array
@@ -13,3 +14,30 @@ ogle_dir = os.path.join(mmap_array.default_array_dir, 'ogle_iii')
 
 def for_subset(name):
     return mmap_array.IndexedArrayDescriptor(base_dir=os.path.join(ogle_dir, name))
+
+
+class OgleDataset(mmap_array.IndexedArray):
+    def __init__(self, index_df, lc_data):
+        super(OgleDataset, self).__init__(index_df, lc_data)
+        self.all_ids = sorted(frozenset(index_df['id'].tolist()))
+
+    def random_id(self):
+        return random.choice(self.all_ids)
+
+    def lcs_for_id(self, id_):
+        """Get light curves for an ID.
+
+        :param id_: ID to get data for.
+        :return: (index_row_namedtuple, sub_array) pairs.
+            sub_array is a [num_points, 3]-shaped array.
+        """
+        row_and_data = []
+        for row in self.index_df[self.index_df['id'] == id_].itertuples():
+            data = self.lc_data[row.start:row.end, :]
+            row_and_data.append((row, data))
+        return row_and_data
+
+    @classmethod
+    def read_for_name(cls, name):
+        ia = for_subset(name).read()
+        return cls(ia.index_df, ia.lc_data)
--- justice/ogle_data_builder.py
+++ justice/ogle_data_builder.py
@@ -9,8 +9,9 @@ import re
 import os
 import os.path
 
-from justice import ogle_data
+import pandas as pd
 
+from justice import ogle_data
 
 # NOTE(gatoatigrado): Add new filename matches as necessary.
 _extract_name = re.compile(r"(OGLE-LMC-CEP-\d+).dat")
@@ -21,7 +22,14 @@ def _dat_files(directory):
     return [(match.group(0), match.group(1)) for match in matches]
 
 
-def build(ogle3_data_dir, ia_descriptor, expected_bands=('I', 'V')):
+def parse_lc_line(line):
+    hjd, mag, mag_err = map(float, line.strip().split())
+    flux = 10.0 ** (-0.4 * mag)
+    flux_err = (mag_err / mag) * flux
+    return hjd, flux, flux_err
+
+
+def build(ogle3_data_dir, ia_descriptor, expected_bands=('I', 'V'), star_table=None):
     """Builds a binary OGLE dataset from original .DAT files.
 
     :param ogle3_data_dir: OGLE-3 data dir.
@@ -29,7 +37,12 @@ def build(ogle3_data_dir, ia_descriptor, expected_bands=('I', 'V')):
     :param ia_descriptor: Descriptor of where to save data.
     :type ia_descriptor: justice.mmap_array.IndexedArrayDescriptor
     :param expected_bands: Expected bands.
+    :param star_table: Star table filename.
     """
+
+    if star_table:
+        star_table = pd.read_csv(star_table, sep="\t").set_index("ID")
+
     band_dirs = [os.path.join(ogle3_data_dir, band) for band in expected_bands]
     for band_dir in band_dirs:
         if not os.path.isdir(band_dir):
@@ -43,25 +56,37 @@ def build(ogle3_data_dir, ia_descriptor, expected_bands=('I', 'V')):
     if not band_names:
         raise ValueError("Expected filenames to be shared.")
 
+    id_str_to_info = {}
+    if star_table is not None:
+        for filename, id_str in band_names:
+            row = star_table.ix[id_str
+                                ]  # If KeyError, does not contain info for all files.
+            id_str_to_info[id_str] = {
+                "field": row["Field"],
+                "StarID": row["StarID"],
+                "Type": row["Type"],
+                "Subtype": row["Subtype"],
+            }
+
     index_rows = []
     data = []
     for filename, id_str in band_names:
         for band, band_dir in zip(expected_bands, band_dirs):
-            index_rows.append({
-                'id': id_str,
-                'band': band,
-            })
+            index_rows.append(dict(
+                id_str_to_info[id_str],
+                id=id_str,
+                band=band,
+            ))
             with open(os.path.join(band_dir, filename)) as f:
-                contents = [
-                    list(map(float,
-                             line.strip().split()))
-                    for line in f.read().strip().splitlines()
-                ]
+                contents = list(map(parse_lc_line, f.read().strip().splitlines()))
             data.append(contents)
 
     ia_descriptor.write(index_rows, data)
-    print("Wrote dataset in {} with {} light curves".format(
-        ia_descriptor.index_filename, len(data)))
+    print(
+        "Wrote dataset in {} with {} light curves".format(
+            ia_descriptor.index_filename, len(data)
+        )
+    )
 
 
 def main():
@@ -69,11 +94,17 @@ def main():
     cmd_args.add_argument("--name", required=True)
     cmd_args.add_argument("--data-directory", required=True)
     cmd_args.add_argument("--expected-bands", default="I,V", help="Expected band names.")
+    cmd_args.add_argument("--star-table")
     args = cmd_args.parse_args()
 
     expected_bands = tuple(args.expected_bands.split(','))
     ia_descriptor = ogle_data.for_subset(args.name)
-    build(args.data_directory, ia_descriptor, expected_bands=expected_bands)
+    build(
+        args.data_directory,
+        ia_descriptor,
+        expected_bands=expected_bands,
+        star_table=args.star_table
+    )
 
 
 if __name__ == '__main__':
