c00c9814b118f6a2e5788085e87479e1eecae716
Alex Malz <aimalz@nyu.edu>
2018-10-07 18:14:57 -0400

    will need these later

---
+++ justice/integratedfilters.py
@@ -0,0 +1,1 @@
+# from https://github.com/lsst/throughputs/tree/master/baselinelsst_filter_integrals = {'u': 0.008839841331651811, 'g': 0.06405273799018851, 'r': 0.07143879389766547, 'i': 0.06425648685231797, 'z': 0.051924583550555285, 'Y': 0.027078250550015438}
