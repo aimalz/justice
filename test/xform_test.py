f6a121ee691a4d873523f310981cb700fd41929a
Ted Singer <tedsinger1@gmail.com>
2018-10-21 17:27:56 -0400

    cleaner

--- test/xform_test.py
+++ test/xform_test.py
@@ -4,12 +4,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-import pytest
 
-from justice import xform, simulate, lightcurve
+from justice import xform, simulate
 from test import lightcurve_test
-from tensorflow.contrib.framework import nest
 
 
 def test_linear_band_data_xform():
@@ -20,6 +17,7 @@ def test_linear_band_data_xform():
     lightcurve_test._assert_near(after.flux, [28, 32])
     lightcurve_test._assert_near(after.flux_err, [2, 2])
 
+
 def test_independent_lc_xforms():
     xf = xform.LinearBandDataXform(1, 2, 3, 4)
     lcxf = xform.IndependentLCXform(b=xf)
@@ -29,11 +27,3 @@ def test_independent_lc_xforms():
     lightcurve_test._assert_near(after.bands['b'].time, [9, 12])
     lightcurve_test._assert_near(after.bands['b'].flux, [28, 32])
     lightcurve_test._assert_near(after.bands['b'].flux_err, [2, 2])
-'''
-def test_transform():
-    tf = xform.Xform(tx=-3215, ty={'a':0.2, 'b':0.2}, dx=10.0, dy={'a':2.0, 'b':2.0}, rs=.1)
-    lca = simulate.TestLC.make_super_easy(time=np.array([3215, 3217]))
-    lc = tf.transform(lca)
-    assert lc.bands['b'].time[0] == 0.0
-    assert lc.bands['b'].time[1] == 20
-'''
