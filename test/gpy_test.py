5100c0eb4c0d2b2af07d24332ccee093cab28186
Ted Singer <tedsinger1@gmail.com>
2018-10-21 17:41:51 -0400

    not sure why this test didnt run locally

--- test/gpy_test.py
+++ test/gpy_test.py
@@ -9,10 +9,11 @@ from justice import xform
 def test_gpy():
     glc = sim.TestLC.make_hard_gauss()
 
-    aff = glc.get_xform([50., 1., 1., 1.5, 1., 1.])
-    glc2 = xform.transform(glc, aff)
+    aff = xform.LinearBandDataXform([50., 1., 1.5, 1.])
+    lcxf = xform.SimultaneousLCXform(aff)
+    glc2 = lcxf.apply(glc)
 
-    aff2 = summ.opt_gp(glc, glc2, vb=False, options={'maxiter': 10})
+    aff2 = summ.opt_alignment(glc, glc2, vb=False, options={'maxiter': 10}, scoretype='gp')
     del aff2  # unused
 
 
@@ -20,11 +21,13 @@ def test_gpy():
 def test_gpy_with_overlap_cost():
     glc = sim.TestLC.make_hard_gauss()
 
-    aff = glc.get_xform([50., 1., 1., 1.5, 1., 1.])
-    glc2 = xform.transform(glc, aff)
+    aff = xform.LinearBandDataXform([50., 1., 1.5, 1.])
+    lcxf = xform.SimultaneousLCXform(aff)
+    glc2 = lcxf.apply(glc)
 
     overlap_cost_fcn = summ.OverlapCostComponent([1.0, 0.1, 0.0], 1.0)
-    aff2 = summ.opt_gp(
-        glc, glc2, vb=False, options={'maxiter': 10}, overlap_cost_fcn=overlap_cost_fcn
+    aff2 = summ.opt_alignment(
+        glc, glc2, vb=False, options={'maxiter': 10},
+        overlap_cost_fcn=overlap_cost_fcn, scoretype='gp'
     )
     del aff2  # unused
