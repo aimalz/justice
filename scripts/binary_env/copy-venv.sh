12adbfb50338006b87df08e9375bb4abda331e79
zora tung <gatoatigrado@gmail.com>
2018-11-23 16:41:59 -0500

    use virtualenv instead of venv for less symlinking :/

--- scripts/binary_env/copy-venv.sh
+++ scripts/binary_env/copy-venv.sh
@@ -6,5 +6,5 @@ set -e
     docker build -t justice-env .
 )
 container_id=$(docker create --name justice-env-container justice-env /bin/bash)
-docker cp "${container_id}":/home/nonroot/justice-venv-py3 justice-venv-py3
+docker cp "${container_id}":/home/nonroot/justice-venv-py3.tar.bz2 justice-venv-py3.tar.bz2
 docker rm "${container_id}"
