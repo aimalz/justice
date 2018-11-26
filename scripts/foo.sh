4f27da626571ebdb0be2f3bacef9b002d591bd09
Alex Malz <aimalz@nyu.edu>
2018-11-25 19:27:54 -0500

    an example of a script

--- scripts/foo.sh
+++ scripts/foo.sh
@@ -2,5 +2,5 @@
 module load python3/intel/3.6.3
 cd /home/$USER/justice
 source /scratch/$USER/venv3/bin/activate
-python3 -m pytest -s -m "not no_precommit" test "$@"
-# python3 period.py
+# python3 -m pytest -s -m "not no_precommit" test "$@"
+python3 testscript.py
