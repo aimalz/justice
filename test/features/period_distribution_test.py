d0e1747997441935084a21d75962e75b5b50fefd
gatoatigrado <gatoatigrado@gmail.com>
2018-12-02 18:16:30 -0500

    Fix test imports and stuff (dunno why it worked on some setups before)

--- test/features/period_distribution_test.py
+++ test/features/period_distribution_test.py
@@ -3,12 +3,15 @@ from justice.datasets import plasticc_data
 from justice.features import period_distribution
 import numpy as np
 
+
+@pytest.mark.slow
 @pytest.mark.requires_real_data
 def test_lomb_scargle():
-    training = plasticc_data.PlasticcDataset.training_data()
-    lcs = plasticc_data.PlasticcDatasetLC.get_lcs_by_target("data/plasticc_training_data.db",92)
+    source = plasticc_data.PlasticcBcolzSource.get_default()
+    lc, = plasticc_data.PlasticcDatasetLC.bcolz_get_lcs_by_obj_ids(
+        bcolz_source=source, dataset="training_set", obj_ids=[615]
+    )
     period_transform = period_distribution.MultiBandLs()
-    lc = lcs[0]
     mbp = period_transform.apply(lc)
     period_best = mbp.best_periods[0]
-    assert np.abs(period_best-.324499300710728) < 1e-5
+    assert np.abs(period_best - .324499300710728) < 1e-5
